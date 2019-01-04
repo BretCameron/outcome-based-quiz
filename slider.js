b96211d2331bfb4a35fdc39b96332a7c31753a77
Bret Cameron <44526664+BretCameron@users.noreply.github.com>
2019-01-04 16:19:59 +0000

    Add files via upload

--- slider.js
+++ slider.js
@@ -1,6 +1,4 @@
-let questions = $('.question');
-
-//ONLY DISPLAY THE FIRST QUESTION
+//DISPLAY THE FIRST QUESTION, ADD QUESTION 1, QUESTION 2, ETC. AS HEADERS
 
 $('.question').each(function (i, val) {
     $(this).addClass('question' + i);
@@ -9,10 +7,11 @@ $('.question').each(function (i, val) {
     };
     if (i < $('.question').length - 1) {
         $(this).prepend(`<h3>Question ${i + 1}</h3>`);
+        $(this).append(`<br>Question ${i + 1} of ${$('.question').length - 1}`);
     };
 });
 
-//SET UP PROGRESS CIRCLES
+//CREATE PROGRESS CIRCLES
 
 for (let i = 0; i < $('.question').length - 1; i++) {
     $('#progress').append('<div class="circle"></div>\n');
@@ -22,42 +21,105 @@ for (let i = 0; i < $('.question').length - 1; i++) {
             $(this).addClass('filled');
         }
     });
-
 };
 
-//NUMBER OF QUESTIONS ANSWERED
+//TURN PAGE (FUNCTION)
 
-let i = 0;
-$('.question' + i).append(`<br>Question ${i + 1} of ${$('.question').length - 1}`).css('font-style', 'italic');
+let pageNum = 0, furthestPage = 0;
+let pageNumHistory = [0];
 
-//AFTER EACH ANSWER, SHOW THE NEXT QUESTION
+const turnPage = () => {
+    $('.question').each(function (i, val) {
+        if (i !== pageNum) {
+            $(this).addClass('display-none');
+        } else {
+            $(this).removeClass('display-none');
+        };
+    });
+    for (let j = 0; j < $('.question').length; j++) {
+        if (j <= pageNum) {
+            $('.circle' + j).addClass('filled');
+        } else {
+            $('.circle' + j).removeClass('filled');
+        }
+    };
+    pageNumHistory.push(pageNum);
+    furthestPage = Math.max(...pageNumHistory);
+
+    //VISUAL CHANGES TO BACK AND FORWARD
+    if (pageNum === 0) {
+        $('#turn-back').addClass('faded');
+    } else {
+        $('#turn-back').removeClass('faded');
+    };
+    if (pageNum === furthestPage) {
+        $('#turn-forward').addClass('faded');
+    } else {
+        $('#turn-forward').removeClass('faded');
+    };
+};
+
+//TURN PAGE (BUTTONS)
+
+$('#turn-back').click(function () {
+    if (pageNum > 0) {
+        pageNum--;
+        turnPage();
+    }
+});
+
+$('#turn-forward').click(function () {
+    if (pageNum < furthestPage) {
+        pageNum++;
+        turnPage();
+    };
+});
+
+$('#reset').click(function () {
+    pageNum = 0;
+    furthestPage = 0;
+    pageNumHistory = [0];
+    turnPage();
+    $('.select').removeClass('select');
+});
 
 $('button').click(function () {
-    if (i < $('.question').length - 1) {
-        $('.question' + (i + 1)).removeClass('display-none');
-        $('.question' + i).addClass('display-none');
-    }
-    $('.circle' + (i + 1)).addClass('filled');
-    i++;
-    if (i <= $('.question').length - 2) {
-        $('.question' + i).append(`<br>Question ${i + 1} of ${$('.question').length - 1}`).css('font-style', 'italic');
-    }
-})
-
-$('#reveal').click(function () {
-    $('#reveal-text').text('Coming Soon!');
-})
+    if (pageNum < $('.question').length - 1) {
+        pageNum++;
+        turnPage();
+    };
+});
 
 //DEFINE OUTCOMES
 
 let outcomes = {
-    outcome1: { name: 'Advanced Programme', count: 0 },
-    outcome2: { name: 'Intermediate Programme', count: 0 },
-    outcome3: { name: 'Beginner Programme', count: 0 }
+    outcome1: { name: 'You mostly hit the top answer', count: 0 },
+    outcome2: { name: 'You mostly hit the middle answers', count: 0 },
+    outcome3: { name: 'You mostly hit the bottom answer', count: 0 }
 };
 
-$('button[class^=outcome]').click(function () {
-    let buttonClass = $(this).attr('class');
-    outcomes[buttonClass].count += 1;
-    console.log(buttonClass + ': ' + outcomes[buttonClass].count);
+//ADD SELECTIONS (ONLY 1 PER QUESTION)
+
+$('button').click(function () {
+    if (!$(this).hasClass('select')) {
+        $(this).parent().find('button').each(function () {
+            $(this).removeClass('select');
+        })
+        $(this).addClass('select');
+    };
+});
+
+//COUNT UP SELECTED BUTTONS
+
+$('#reveal').click(function () {
+    let outcomesArray = Object.keys(outcomes);
+    //RESET COUNTS TO ZERO
+    let scoresArray = [];
+    //TALLY UP HOW MANY OF EACH OUTCOME IS SELECTED
+    outcomesArray.forEach(function (element) {
+        let score = $('.select.' + element).length;
+        scoresArray.push(score);
+    });
+    let winner = (outcomesArray[scoresArray.indexOf(Math.max(...scoresArray))]);
+    $('#reveal-text').text(outcomes[winner].name);
 });
