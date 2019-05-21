3f793e3eed3229adc16ec391a79a0dc3b5820a38
Bret Cameron <44526664+BretCameron@users.noreply.github.com>
2019-05-21 10:05:08 +0100

    Add postMessage on window load

--- events.js
+++ events.js
@@ -1,9 +1,13 @@
 let height = document.querySelector('body').offsetHeight;
 
+window.onload = () => {
+  height = document.querySelector('body').offsetHeight;
+  window.parent.postMessage({ frameHeight: height }, '*');
+}
+
 window.onresize = () => {
   if (height !== document.querySelector('body').offsetHeight) {
     height = document.querySelector('body').offsetHeight;
     window.parent.postMessage({ frameHeight: height }, '*');
-    console.log(height);
   }
 }
